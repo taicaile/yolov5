6f18b803da60a24c425d381e83aaebc72a50a519
neil <diycar@foxmail.com>
2021-04-14 15:49:59 +0800

    update

--- utils/plots.py
+++ utils/plots.py
@@ -59,7 +59,7 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     # tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     if isinstance(x[0], torch.Tensor):
         x = [_x.item() for _x in x]
-    tl = line_thickness or math.floor(0.01 * min(abs(x[2]-x[0]), abs(x[3]-x[1]))) + 1  # line/font thickness
+    tl = math.floor(0.01 * min(abs(x[2]-x[0]), abs(x[3]-x[1]))) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
