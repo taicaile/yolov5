9725d4c20533b4bbc0f5d4fefd986943f5de10f7
Neil <diycar@foxmail.com>
2021-06-29 17:25:18 +0800

    update

--- models/common.py
+++ models/common.py
@@ -105,13 +105,13 @@ class Bottleneck(nn.Module):
 
 class SELayer(nn.Module):
     def __init__(self, channel, e=1.0):
-        super(SELayer, self).__init__()
+        super().__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        reduction = 16 * e
+        hidden_channels = max(1, int(channel*e/16))
         self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
+            nn.Linear(channel, hidden_channels, bias=False),
             nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Linear(hidden_channels, channel, bias=False),
             nn.Sigmoid()
         )
 
@@ -124,7 +124,7 @@ class SELayer(nn.Module):
 class BottleneckSE(nn.Module):
     # bottleneck with SELayer
     def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
-        super(Bottleneck, self).__init__()
+        super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c_, c2, 3, 1, g=g)
