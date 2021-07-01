5223d1c5bdf3f236e87799f814d421a08be11020
Neil <diycar@foxmail.com>
2021-07-01 15:17:01 +0800

    update

--- models/common.py
+++ models/common.py
@@ -119,12 +119,12 @@ class SELayer(nn.Module):
         b, c, _, _ = x.size()
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
-        return x * y.expand_as(x) 
+        return x * y.expand_as(x)
 
 class BottleneckSE(nn.Module):
     # bottleneck with SELayer
     def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
-        super(Bottleneck, self).__init__()
+        super(BottleneckSE, self).__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c_, c2, 3, 1, g=g, act=False)
